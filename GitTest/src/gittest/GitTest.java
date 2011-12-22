d01a15d5a54581e0dff6fec87a28d8d1465e7bfd
unknown <Gennaro@gennaro-9447b3b.(none)>
2011-12-22 14:16:09 -0700

    Netbeans project added

---
+++ GitTest/src/gittest/GitTest.java
@@ -0,0 +1,19 @@
+
+/*--------------------------------------------------------------------------*/
+/*
+ * Author:  Gennaro
+ * Date:    Dec 22, 2011
+ * Purpose: 
+ * Inputs:  
+ * Outputs: 
+ */
+package gittest;
+
+//imports
+public class GitTest {
+
+//variables
+    public static void main(String[] args) {
+        
+    }
+}
